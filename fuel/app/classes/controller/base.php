cade164ed50b39c8c93b0f602284766a76756e4c
ida <ida@AFN-hakenA>
2014-03-14 09:44:33 +0900

    refs #596 プロパティがキャメルからスネークに変更

--- fuel/app/classes/controller/base.php
+++ fuel/app/classes/controller/base.php
@@ -12,21 +12,21 @@ class Controller_Base extends Controller_Template
      *
      * @var array
      */
-    protected $postActions = array();
+    protected $post_actions = array();
 
     /**
      * ログインが必須のアクション配列
      *
      * @var array
      */
-    protected $loginActions = array();
+    protected $login_actions = array();
 
     /**
      * 未ログインOKのアクション配列
      *
      * @var array
      */
-    protected $nologinActions = array();
+    protected $nologin_actions = array();
 
     /**
      * 事前処理
@@ -44,16 +44,16 @@ class Controller_Base extends Controller_Template
         $action = Request::active()->action;
 
         if (Input::method() !== 'POST'
-            && in_array($action, $this->postActions)
+            && in_array($action, $this->post_actions)
         ) {
             Response::redirect('errors/badrequest');
         }
 
-        if (in_array($action, $this->loginActions) && !Auth::check()) {
+        if (in_array($action, $this->login_actions) && !Auth::check()) {
             Response::redirect('auth/login');
         }
 
-        if (in_array($action, $this->nologinActions) && Auth::check()) {
+        if (in_array($action, $this->nologin_actions) && Auth::check()) {
             Response::redirect('auth/logined');
         }
     }
