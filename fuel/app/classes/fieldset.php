416271b5af8570cdc067d7f8095ef96117525bec
Ricky <master@mistdev.com>
2014-03-21 16:46:56 +0900

    不要なフィールドセット拡張のメソッドを削除

--- fuel/app/classes/fieldset.php
+++ fuel/app/classes/fieldset.php
@@ -2,8 +2,8 @@
 
 /**
  * フィールドセットの拡張。
- * validatedの拡張以外あまりまだ活用されていません。
  *
+ * @author shimma
  */
 class Fieldset extends Fuel\Core\Fieldset
 {
@@ -25,108 +25,6 @@ class Fieldset extends Fuel\Core\Fieldset
         }
 
         return $value;
-    }
-
-    /**
-     * フォーム要素をviewで使いやすい配列で取得
-     *
-     * @todo あまり活用されていないので、不要であれば削除
-     * @param  string $open
-     * @param  array  $hidden
-     * @access public
-     * @return void
-     * @author shimma
-     */
-    public function getFormElements($open = '', $hidden = array())
-    {
-        if (is_array($open)) {
-            $attr = $open;
-        } else {
-            $attr['action'] = $open;
-            $attr['method'] = 'post';
-        }
-
-        $attr['id'] = 'form_' . $this->get_name();
-
-        //常にhiddenパラメータでCSRF対策のトークンを付与するようにする
-        $hidden[Config::get('security.csrf_token_key')] = Security::fetch_token();
-        $form['open'] = $this->form()->open($attr, $hidden);
-
-        foreach ($this->field() as $f) {
-            $form[$f->name] = array('label' => $f->label, 'html' => $f->build());
-        }
-
-        $form['close'] = '</form>';
-        $form['error'] = $this->show_errors();
-
-        return $form;
-    }
-
-    /**
-     * 数値の入力を受け付けるinput text要素
-     * - 自動的に数値バリデーションが追加される
-     * - 自動的にime-modeがdisabledに設定される
-     *
-     * @param  mixed $name
-     * @param  mixed $label
-     * @access public
-     * @return void
-     * @author shimma
-     */
-    public function addTextForNumeric($name, $label)
-    {
-        return $this->add(
-            $name,
-            $label,
-            array(
-                'class' => 'input-medium',
-                'style' => 'ime-mode:disabled'
-            )
-        )->add_rule('valid_string', 'numeric')->set_template('{field}');
-    }
-
-    /**
-     * 選択肢を一行で表示するRadio要素
-     *
-     * @param  mixed $name
-     * @param  mixed $label
-     * @param  mixed $options
-     * @access public
-     * @return void
-     * @author shimma
-     */
-    public function addRadioInline($name, $label, $options)
-    {
-        return $this->add(
-            $name,
-            $label,
-            array(
-                'type'    => 'radio',
-                'options' => $options,
-            )
-        )->set_template('{fields}<label class="radio inline">{field}{label}</label>{fields}');
-    }
-
-    /**
-     * 選択肢を改行するRadio要素
-     *
-     * @param  mixed $name
-     * @param  mixed $label
-     * @param  mixed $options
-     * @access public
-     * @return void
-     * @author shimma
-     */
-    public function addRadioWithBr($name, $label, $options)
-    {
-        return $this->add(
-            $name,
-            $label,
-            array(
-                'type'    => 'radio',
-                'options' => $options,
-            )
-        )->set_template('{fields}<label class="radio">{field}{label}</label>{fields}');
     }
 
 }
