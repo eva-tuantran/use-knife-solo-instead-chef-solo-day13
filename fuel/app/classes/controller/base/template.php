2e73060e682a63ca0449816fcf2fbed2ab477188
Ricky <master@mistdev.com>
2014-03-26 13:54:19 +0900

    refs #315 ユーザの確認メール送信をlangのtemplate経由から送信するように変更

--- fuel/app/classes/controller/base/template.php
+++ fuel/app/classes/controller/base/template.php
@@ -126,48 +126,6 @@ class Controller_Base_Template extends Controller_Template
     }
 
     /**
-     * ユーザーにメールを送信
-     *
-     * @para $name メールの識別子 $params 差し込むデータ $to 送り先(指定しなければ langの値を使用)
-     * @access protected
-     * @return void
-     * @author kobayasi
-     */
-    protected function sendMailByParams($name,$params = array(), $to = null)
-    {
-        Lang::load("email/{$name}");
-
-        $email = Email::forge();
-        $email->from(Lang::get('from'),Lang::get('from_name'));
-        $email->subject(Lang::get('subject'));
-        $email->body($this->createMailBody(Lang::get('body'),$params));
-
-        if (! $to) {
-            $to = Lang::get('email');
-        }
-
-        $email->to($to);
-        $email->send();
-    }
-
-    /**
-     * メール本文の作成
-     *
-     * @para $contact Model_Contact
-     * @access protected
-     * @return string
-     * @author kobayasi
-     */
-    private function createMailBody($body,$params)
-    {
-        foreach ( $params as $key => $value ) {
-            $body = str_replace("##{$key}##",$value,$body);
-        }
-        return mb_convert_encoding($body,'jis');
-    }
-
-
-    /**
      * ステータス変更文字列を取得します。
      *
      * @param int $i
