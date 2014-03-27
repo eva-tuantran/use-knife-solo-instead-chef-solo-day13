092aaf77c1ee71e508acee990202f8d811415ba5
Ricky <master@mistdev.com>
2014-03-27 20:12:48 +0900

    php oil r seed にて指定されたファイルを全部取り込むように変更

--- fuel/app/tasks/seed.php
+++ fuel/app/tasks/seed.php
@@ -2,12 +2,30 @@
 
 namespace Fuel\Tasks;
 
+/**
+ * seed
+ *
+ * @author shimma
+ * @todo arrayで指定しているものを一括で取り込むようにしました。従来通り一個ずつの対応の場合は修正お願いします。
+ */
 class seed
 {
-    public function run($model)
+    public function run()
     {
-        if (!empty($model)) {
+        $models = array(
+            'user',
+            'entry',
+            'fleamarket',
+        );
+        foreach ($models as $model) {
             include (APPPATH . 'seeds/' . \Fuel::$env . '/' . $model. '.php');
         }
     }
+
+    // public function run($model = "")
+    // {
+        // if (! empty($model)) {
+            // include (APPPATH . 'seeds/' . \Fuel::$env . '/' . $model. '.php');
+        // }
+    // }
 }
