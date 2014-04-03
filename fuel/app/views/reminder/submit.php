40e0e9939ed050599ebbcaf30f4ea82f992df7f4
Ricky <master@mistdev.com>
2014-04-03 23:02:42 +0900

    refs #964 送信からパスワード変更までひと通り動くところまで完成

---
+++ fuel/app/views/reminder/submit.php
@@ -0,0 +1,21 @@
+<div id="contentLogin" class="row">
+  <!-- ad -->
+  <div id="ad" class="col-sm-2">
+    <!-- <a href="#"><img src="http://dummyimage.com/535x250/ccc/fff.jpg"  class="img-responsive"></a> -->
+  </div>
+  <!-- /ad -->
+  <!-- loginForm -->
+  <div id="loginForm" class="col-sm-7">
+    <div class="box clearfix">
+      <h3>パスワードを忘れた方</h3>
+      <p style="margin-bottom: 20px;">パスワードリセットメールをしました。メールボックスをご確認下さい。<br />
+      <p>メールが届かない場合は以下をご確認下さい。</p>
+      <ul style="padding-left: 20px; padding-top: 0px; list-style: disc;">
+        <li>迷惑メールフォルダに入っている</li>
+        <li>rakuichi-rakuza.jpがメールのドメイン許可設定されていない</li>
+        <li>迷惑メールフォルダに入っている</li>
+      </ul>
+    </div>
+  </div>
+  <!-- /loginForm -->
+</div>
