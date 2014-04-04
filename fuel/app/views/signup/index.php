61258da7e08ea6571e420e9e7d496234151468bb
Kazunari.NAKATA <nakata@aucfan.com>
2014-04-04 11:59:44 +0900

    会員登録時の住所自動入力バグ修正

--- fuel/app/views/signup/index.php
+++ fuel/app/views/signup/index.php
@@ -24,8 +24,8 @@
         <div class="form-group">
           <label class="col-sm-2 control-label" for="inputName">お名前</label>
           <div class="col-sm-10">
-          <input type="text" class="form-control" id="inputName_last" placeholder="姓を入力" name="last_name" style="width: 10em; float: left; margin-right: 10px;" value="<?php echo $input['last_name'] ?>" />
-          <input type="text" class="form-control" id="inputName_first" placeholder="名を入力" name="first_name" style="width: 10em;" value="<?php echo $input['first_name'] ?>" />
+            <input type="text" class="form-control" id="inputName_last" placeholder="姓を入力" name="last_name" style="width: 10em; float: left; margin-right: 10px;" value="<?php echo $input['last_name'] ?>" />
+            <input type="text" class="form-control" id="inputName_first" placeholder="名を入力" name="first_name" style="width: 10em;" value="<?php echo $input['first_name'] ?>" />
           </div>
         </div>
         <div class="form-group">
@@ -39,14 +39,14 @@
           <label class="col-sm-2 control-label" for="inputAddress">ご住所</label>
           <div class="col-sm-10">
             <input type="text" class="form-control" id="inputZip" placeholder="例）123-4567" name="zip" />
-            <button type="submit" class="btn btn-default" onclick="AjaxZip3.zip2addr('zip','','address','address'); return false;">住所を検索</button>
+            <button type="submit" class="btn btn-default" onclick="AjaxZip3.zip2addr('zip','','prefecture_id','address'); return false;">住所を検索</button>
             <select class="form-control" name="prefecture_id">
-                <option>都道府県</option>
-                  <?php foreach ($prefectures as $prefecture_id => $name): ?>
-                    <option value="<?php echo $prefecture_id; ?>" <?php if($input['prefecture_id'] == $prefecture_id):?>selected<?php endif; ?>><?php echo $name; ?></option>
-                  <?php endforeach; ?>
+              <option>都道府県</option>
+              <?php foreach ($prefectures as $prefecture_id => $name): ?>
+                <option value="<?php echo $prefecture_id; ?>" <?php if($input['prefecture_id'] == $prefecture_id):?>selected<?php endif; ?> name='pref01'><?php echo $name; ?></option>
+              <?php endforeach; ?>
             </select>
-                <input type="text" class="form-control" id="inputAddress" placeholder="住所を入力" name="address" value="<?php echo $input['address'] ?>" />
+            <input type="text" class="form-control" id="inputAddress" placeholder="住所を入力" name="address" value="<?php echo $input['address'] ?>" />
           </div>
         </div>
         <div class="form-group">
@@ -64,46 +64,46 @@
         <div class="form-group">
           <label class="col-sm-2 control-label" for="inputEmail2">E-mailアドレス<br>
             （確認用）</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail2" placeholder="確認のため、もう一度メールアドレスを入力してください" name="email2" value="<?php echo $input['email2'] ?>" />
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label" for="inputPassword">パスワード</label>
-          <div class="col-sm-10">
-          <input type="password" class="form-control" id="inputPassword" placeholder="半角英数字6文字以上で入力してください" name="password" value="<?php echo $input['password'] ?>"/>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label" for="inputPassword2">パスワード<br>
-            （確認用）</label>
-          <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword2" placeholder="確認のため、もう一度パスワードを入力してください" name="password2" value="<?php echo $input['password2'] ?>" />
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label" for="inputNickname">ニックネーム</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputNickname" placeholder="ニックネームを入力" name="nick_name" value="<?php echo $input['nick_name'] ?>" />
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label" for="terms">利用規約</label>
-          <div class="col-sm-10">
-            <div class="checkbox">
-              <label>
-              <input type="checkbox" name="terms" value="agree" <?php if(isset($input['terms'])):?>checked<?php endif ?>>
-                利用規約に同意する。 <br>
-                <a href="#" target="_blank">規約の確認はコチラ（別ウィンドウで開きます）</a> </label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="inputEmail2" placeholder="確認のため、もう一度メールアドレスを入力してください" name="email2" value="<?php echo $input['email2'] ?>" />
             </div>
           </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="inputPassword">パスワード</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control" id="inputPassword" placeholder="半角英数字6文字以上で入力してください" name="password" value="<?php echo $input['password'] ?>"/>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="inputPassword2">パスワード<br>
+              （確認用）</label>
+              <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword2" placeholder="確認のため、もう一度パスワードを入力してください" name="password2" value="<?php echo $input['password2'] ?>" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-2 control-label" for="inputNickname">ニックネーム</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputNickname" placeholder="ニックネームを入力" name="nick_name" value="<?php echo $input['nick_name'] ?>" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-2 control-label" for="terms">利用規約</label>
+              <div class="col-sm-10">
+                <div class="checkbox">
+                  <label>
+                    <input type="checkbox" name="terms" value="agree" <?php if(isset($input['terms'])):?>checked<?php endif ?>>
+                    利用規約に同意する。 <br>
+                    <a href="#" target="_blank">規約の確認はコチラ（別ウィンドウで開きます）</a> </label>
+                  </div>
+                </div>
+              </div>
+              <div id="submitButton" class="form-group">
+                <button type="submit" class="btn btn-default">内容を確認する</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div id="submitButton" class="form-group">
-          <button type="submit" class="btn btn-default">内容を確認する</button>
-        </div>
-      </form>
-    </div>
-  </div>
-  <!-- /form -->
-</div>
+        <!-- /form -->
+      </div>
 
