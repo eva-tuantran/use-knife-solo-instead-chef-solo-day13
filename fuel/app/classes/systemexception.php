ce21826cb2cc9c7d46bab0357172739bedcdbb1f
ida <ida@AFN-hakenA>
2014-04-08 09:57:32 +0900

    refs #842 SystemExceptionで未定義エラー対応

--- fuel/app/classes/systemexception.php
+++ fuel/app/classes/systemexception.php
@@ -7,17 +7,26 @@
 
 class SystemException extends \FuelException
 {
-	public function response()
+    public function __construct($message = '', $code = 0, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function response()
 	{
         $error_code = $this->getMessage();
-        $errors = Lang::load('error', $error_code);
+        $error_list = Lang::load('error', $error_code);
 
+        if (! isset($error_list[$error_code])) {
+            $error_code = 0;
+        }
+
+        $params = array(
+            'error_code' => $error_code,
+            'error_message' => $error_list[$error_code],
+        );
         $response = \Request::forge('errors/index', false)
-            ->execute(array(
-                'error_code' => $error_code,
-                'error_message' => $errors[$error_code])
-            )
-            ->response();
+            ->execute($params)->response();
 
 		return $response;
 	}
