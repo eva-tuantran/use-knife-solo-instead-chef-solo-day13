0f9131b1e5c6b8e2156830d7feb413bb4e211858
Kazunari.NAKATA <o9o64456583@aucfan.com>
2014-04-09 17:17:31 +0900

    refs #1042 静的ページ作成

--- fuel/app/classes/controller/info.php
+++ fuel/app/classes/controller/info.php
@@ -27,4 +27,40 @@ class Controller_Info extends Controller_Base_Template
     }
 
 
+    public function action_food()
+    {
+        $this->template->content = View::forge('info/food');
+    }
+
+
+    public function action_corporation()
+    {
+        $this->template->content = View::forge('info/corporation');
+    }
+
+
+    public function action_visitor()
+    {
+        $this->template->content = View::forge('info/visitor');
+    }
+
+
+    public function action_agreement()
+    {
+        $this->template->content = View::forge('info/agreement');
+    }
+
+
+    public function action_manager()
+    {
+        $this->template->content = View::forge('info/manager');
+    }
+
+
+    public function action_policy()
+    {
+        $this->template->content = View::forge('info/policy');
+    }
+
+
 }
---
+++ fuel/app/views/info/manager.php
@@ -0,0 +1,25 @@
+<div id="contentInfo" class="row">
+  <!-- form -->
+  <div id="info" class="container">
+    <div class="box clearfix">
+      <h3>運営者情報</h3>
+
+      <h2>会社名</h2>
+      <p>株式会社オークファン</p>
+
+      <h2>事務所</h2>
+      <p>〒150-0043 東京都渋谷区道玄坂1-14-6 ヒューマックス渋谷ビル6階</p>
+
+
+      <h2>TEL</h2>
+      <p>03-6416-3652</p>
+
+
+      <h2>ホームページ</h2>
+      <p><a href="http://aucfan.co.jp/">http://aucfan.co.jp/</a></p>
+
+
+  </div>
+</div>
+<!-- /form -->
+</div>
