45b453bc1a9dde63f2e7741fbcbc21b0e649cd07
Hiroyuki Kobayashi <h_kobayashi@aucfan.com>
2014-04-09 17:27:48 +0900

    refs #1043 フリマ予約をSystemExceptionでエラーに

--- fuel/app/classes/controller/reservation.php
+++ fuel/app/classes/controller/reservation.php
@@ -23,14 +23,14 @@ class Controller_Reservation extends Controller_Base_Template
 
         $this->fieldset = $this->getFieldset();
         if (! $this->fieldset) {
-            return Response::redirect('/');
+            throw new SystemException('ER00502');
         }
 
         $input = $this->fieldset->input();
         $fleamarket = Model_Fleamarket::find($input['fleamarket_id']);
 
         if (! $fleamarket) {
-            return Response::redirect('/');
+            throw new SystemException('ER00502');
         }
 
         $this->fleamarket = $fleamarket;
@@ -98,11 +98,16 @@ class Controller_Reservation extends Controller_Base_Template
 
         try {
             $entry = $this->registerEntry();
+        } catch (Exception $e) {
+            throw new SystemException('ER00501');
+        }
+
+        try {
             $this->sendMailToUser($entry);
         } catch (Exception $e) {
-            $view->set('error', $e, false);
-            throw $e;
+            throw new SystemException('ER00503');
         }
+
         $view->set('entry', $entry, false);
     }
 
