05b55c32f14190c3dd56696d55d33955067c064b
Kazunari.NAKATA <o9o64456583@aucfan.com>
2014-04-16 12:17:28 +0900

    refs #842 エラーモデル変更

--- fuel/app/classes/model/error.php
+++ fuel/app/classes/model/error.php
@@ -8,31 +8,31 @@
 class Model_Error extends \Model
 {
 
-    const ER00101 = \Model_Error::ER00101;
+    const ER00101 = 'ER00101';
 
-    const ER00201 = \Model_Error::ER00201;
-    const ER00202 = \Model_Error::ER00202;
+    const ER00201 = 'ER00201';
+    const ER00202 = 'ER00202';
 
-    const ER00301 = \Model_Error::ER00301;
-    const ER00302 = \Model_Error::ER00302;
-    const ER00303 = \Model_Error::ER00303;
-    const ER00304 = \Model_Error::ER00304;
-    const ER00305 = \Model_Error::ER00305;
+    const ER00301 = 'ER00301';
+    const ER00302 = 'ER00302';
+    const ER00303 = 'ER00303';
+    const ER00304 = 'ER00304';
+    const ER00305 = 'ER00305';
 
-    const ER00401 = \Model_Error::ER00401;
-    const ER00402 = \Model_Error::ER00402;
-    const ER00403 = \Model_Error::ER00403;
-    const ER00404 = \Model_Error::ER00404;
-    const ER00405 = \Model_Error::ER00405;
-    const ER00406 = \Model_Error::ER00406;
+    const ER00401 = 'ER00401';
+    const ER00402 = 'ER00402';
+    const ER00403 = 'ER00403';
+    const ER00404 = 'ER00404';
+    const ER00405 = 'ER00405';
+    const ER00406 = 'ER00406';
 
-    const ER00601 = \Model_Error::ER00601;
-    const ER00602 = \Model_Error::ER00602;
-    const ER00603 = \Model_Error::ER00603;
-    const ER00604 = \Model_Error::ER00604;
-    const ER00605 = \Model_Error::ER00605;
+    const ER00601 = 'ER00601';
+    const ER00602 = 'ER00602';
+    const ER00603 = 'ER00603';
+    const ER00604 = 'ER00604';
+    const ER00605 = 'ER00605';
 
-    const ER00701 = \Model_Error::ER00701;
-    const ER00702 = \Model_Error::ER00702;
+    const ER00701 = 'ER00701';
+    const ER00702 = 'ER00702';
 
 }
