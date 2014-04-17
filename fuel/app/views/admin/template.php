7a14e6f122bf529c0effb3a1ed1ec110d28874ab
Hiroyuki Kobayashi <h_kobayashi@aucfan.com>
2014-04-17 07:07:00 +0000

    管理画面のテンプレート修正

--- fuel/app/views/admin/template.php
+++ fuel/app/views/admin/template.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-<meta charset="utf-8">
-<script src="/assets/js/jquery.min.js"></script>
-</head>
-<body>
-<head>
-  <script src="/assets/js/jquery.min.js"></script>
-  <script src="/assets/js/bootstrap.min.js"></script>
-  <?php echo Asset::render('add_css');?>
-  <?php echo Asset::render('add_js');?>
-</head>
-<body>
-  <a href="/admin/user/list">ユーザー</a>
-  <a href="/admin/fleamarket/list">開催</a>
-  <hr>
-  <?php echo $content; ?>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <?php echo Asset::render('add_css');?>
+    <?php echo Asset::render('add_js');?>
+  </head>
+  <body>
+    <a href="/admin/user/list">ユーザー</a>
+    <a href="/admin/fleamarket/list">開催</a>
+    <hr>
+    <?php echo $content; ?>
+  </body>
 </html>
