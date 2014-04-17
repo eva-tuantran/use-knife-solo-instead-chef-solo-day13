f1b611d8da8bfd2ca86b34f10a891fab037f879b
Ricky <master@mistdev.com>
2014-04-17 20:46:56 +0900

    dbの更新で動かなくなってしまったseedを調整

--- db/rakuichi-rakuza.index.sql
+++ db/rakuichi-rakuza.index.sql
@@ -1,4 +1,4 @@
-CREATE INDEX idx_fleamarkets_01 ON `fleamarkets`(`event_date`, `deleted_at`);
-CREATE INDEX idx_fleamarket_abouts_01 ON `fleamarket_abouts`(`fleamarket_id`, `about_id`);
-CREATE INDEX idx_fleamarket_entry_styles_01 ON `fleamarket_entry_styles`(`fleamarket_id`, `entry_style_id`);
-CREATE INDEX idx_locations_01 ON `locations`(`location_id`, `prefecture_id`);
+CREATE INDEX idx_fleamarkets_01 ON `rakuichi-rakuza`.`fleamarkets`(`event_date`, `deleted_at`);
+CREATE INDEX idx_fleamarket_abouts_01 ON `rakuichi-rakuza`.`fleamarket_abouts`(`fleamarket_id`, `about_id`);
+CREATE INDEX idx_fleamarket_entry_styles_01 ON `rakuichi-rakuza`.`fleamarket_entry_styles`(`fleamarket_id`, `entry_style_id`);
+CREATE INDEX idx_locations_01 ON `rakuichi-rakuza`.`locations`(`location_id`, `prefecture_id`);
