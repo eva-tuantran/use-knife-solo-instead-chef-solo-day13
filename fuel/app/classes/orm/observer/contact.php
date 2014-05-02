52daa6e4b014172c69414d35ef83afebcad0f9eb
Ricky <master@mistdev.com>
2014-05-02 21:29:17 +0900

    今まで未送信であったお問い合わせメールを一括で送信するtasks作成

--- fuel/app/classes/orm/observer/contact.php
+++ fuel/app/classes/orm/observer/contact.php
@@ -10,6 +10,6 @@ class Observer_Contact extends Observer
     }
     public function after_load(Model $contact)
     {
-        $contact->inquiry_type_label = Model_Contact::inquiry_type_label($contact->inquiry_type);
+        $contact->inquiry_type_label = \Model_Contact::inquiry_type_to_label($contact->inquiry_type);
     }
 }
---
+++ fuel/app/tasks/temporary_inquiry_send.php
@@ -0,0 +1,40 @@
+<?php
+namespace Fuel\Tasks;
+
+use \Model_Email;
+use \Model_Contact;
+
+
+class temporary_inquiry_send
+{
+    public function run()
+    {
+        $contacts = \Model_Contact::find('all');
+
+        foreach ($contacts as $contact) {
+            $this->sendMailToAdmin($contact);
+        }
+
+    }
+
+
+    /**
+     * ユーザーにメールを送信
+     *
+     * @para $contact Model_Contact
+     * @access private
+     * @return void
+     */
+    private function sendMailToAdmin($contact)
+    {
+        $params = array();
+
+        foreach (array('last_name', 'first_name', 'subject', 'email', 'tel', 'contents') as $key) {
+            $params[$key] = $contact->get($key);
+        }
+        $params['inquiry_type_label'] = $contact->inquiry_type_label();
+
+        $email = new Model_Email();
+        $email->sendMailByParams("inquiry_admin", $params);
+    }
+}
