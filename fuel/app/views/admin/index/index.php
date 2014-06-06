221f0f00aedb47ed57e9a97da793c24ab48a1586
ida <ida@AFN-P092>
2014-06-06 09:49:03 +0900

    refs #1917 管理画面TOPにメルマガ会員数表示を追加

--- fuel/app/views/admin/index/index.php
+++ fuel/app/views/admin/index/index.php
@@ -1,6 +1,10 @@
 <div class="center-section">
   <ul class="list-group">
     <li class="list-group-item">
+      <span class="badge"><?php echo $mm_member_count;?></span>
+      メルマガ会員数
+    </li>
+    <li class="list-group-item">
       <span class="badge"><?php echo $new_member_count;?></span>
       昨日の新規会員数
     </li>
