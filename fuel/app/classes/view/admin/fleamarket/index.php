99335941cc3e076384f3ee9e6c696700206d8584
ida <ida@AFN-P092>
2014-06-10 12:02:32 +0900

    refs #1960 反響項目を登録しても反映されない

--- fuel/app/classes/view/admin/fleamarket/index.php
+++ fuel/app/classes/view/admin/fleamarket/index.php
@@ -16,9 +16,31 @@ class View_Admin_Fleamarket_Index extends \ViewModel
      */
     public function view()
     {
+        $this->fleamarket_id = \Input::param('fleamarket_id');
         $this->prefectures = \Config::get('master.prefectures');
         $this->entry_styles = \Config::get('master.entry_styles');
         $this->event_statuses = \Model_Fleamarket::getEventStatuses();
         $this->locations = \Model_Location::find('all');
+        $this->link_from_list = $this->getLinkFromList();
+    }
+
+    /**
+     * 反響項目を取得する
+     *
+     * @access private
+     * @param
+     * @return array
+     * @author ida
+     */
+    private function getLinkFromList()
+    {
+        if (! empty($this->fleamarket_id)) {
+            $link_from_list = $this->fieldsets['fleamarket']->field('link_from_list');
+            $result = \Model_Fleamarket::explodeLinkFromList($link_from_list->value);
+        } else {
+            $result = \Model_Entry::getLinkFromList();
+        }
+
+        return $result;
     }
 }
