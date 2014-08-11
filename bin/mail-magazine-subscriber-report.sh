75f8e8d65b4d4328914aedce346c7d561f92c410
root <root@ceb01.aucfan.com>
2014-08-11 15:58:55 +0900

    メールの飛び先をマーケティングMLに変更

--- bin/mail-magazine-subscriber-report.sh
+++ bin/mail-magazine-subscriber-report.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mail_to=shimma@aucfan.com
+mail_to=afml_mkt@aucfan.com
 subject="rakuichi rakuza mailmagazine active user report"
 
 echo "SELECT COUNT(*) AS active_mail_magazine_subscribers FROM users WHERE mm_flag = 1 AND mm_error_flag != 1" \
