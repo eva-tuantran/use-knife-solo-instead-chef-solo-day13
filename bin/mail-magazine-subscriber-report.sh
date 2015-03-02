3bbd40da9e01b67835d320d5c102a7981c0a1c10
aucfan-noguchi <noguchi@aucfan.com>
2015-03-02 14:18:37 +0900

    db -> rds

--- bin/mail-magazine-subscriber-report.sh
+++ bin/mail-magazine-subscriber-report.sh
@@ -3,7 +3,7 @@
 mail_to=afml_mkt@aucfan.com
 subject="rakuichi rakuza mailmagazine active user report"
 
-active_mail_magazine_subscribers=$(echo "SELECT COUNT(*) FROM users WHERE mm_flag = 1 AND mm_error_flag != 1 AND deleted_at IS NULL" | mysql -uroot rakuichi_rakuza -N)
-rakuichi_rakuza_user_all=$(echo "SELECT COUNT(*) FROM users WHERE deleted_at IS NULL" | mysql -uroot rakuichi_rakuza -N)
+active_mail_magazine_subscribers=$(echo "SELECT COUNT(*) FROM users WHERE mm_flag = 1 AND mm_error_flag != 1 AND deleted_at IS NULL" | mysql -uroot -h rakuichi-rakuza.cnceg4b5jglv.ap-northeast-1.rds.amazonaws.com rakuichi_rakuza -N)
+rakuichi_rakuza_user_all=$(echo "SELECT COUNT(*) FROM users WHERE deleted_at IS NULL" | mysql -uroot -h rakuichi-rakuza.cnceg4b5jglv.ap-northeast-1.rds.amazonaws.com rakuichi_rakuza -N)
 
 echo -e "active_mail_magazine_subscribers:${active_mail_magazine_subscribers}\nall_users:${rakuichi_rakuza_user_all}" |  mail -s "$subject" $mail_to
