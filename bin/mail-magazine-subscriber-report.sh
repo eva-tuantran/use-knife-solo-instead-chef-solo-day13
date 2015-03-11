d74521959c4baef654403697762a0adbd0e40939
aucfan-noguchi <noguchi@aucfan.com>
2015-03-11 15:34:12 +0900

    db_userchange

--- bin/mail-magazine-subscriber-report.sh
+++ bin/mail-magazine-subscriber-report.sh
@@ -2,8 +2,11 @@
 
 mail_to=afml_mkt@aucfan.com
 subject="rakuichi rakuza mailmagazine active user report"
+user=rr_admin
+pass=f8f4316c0ad0ce4939fd2966b9aff60e271ffe68
+rds=rakuichi-rakuza.cnceg4b5jglv.ap-northeast-1.rds.amazonaws.com
 
-active_mail_magazine_subscribers=$(echo "SELECT COUNT(*) FROM users WHERE mm_flag = 1 AND mm_error_flag != 1 AND deleted_at IS NULL" | mysql -uroot -h rakuichi-rakuza.cnceg4b5jglv.ap-northeast-1.rds.amazonaws.com rakuichi_rakuza -N)
-rakuichi_rakuza_user_all=$(echo "SELECT COUNT(*) FROM users WHERE deleted_at IS NULL" | mysql -uroot -h rakuichi-rakuza.cnceg4b5jglv.ap-northeast-1.rds.amazonaws.com rakuichi_rakuza -N)
+active_mail_magazine_subscribers=$(echo "SELECT COUNT(*) FROM users WHERE mm_flag = 1 AND mm_error_flag != 1 AND deleted_at IS NULL" | mysql -u${user} -p${pass} -h $rds rakuichi_rakuza -N)
+rakuichi_rakuza_user_all=$(echo "SELECT COUNT(*) FROM users WHERE deleted_at IS NULL" | mysql -u${user} -p${pass} -h $rds rakuichi_rakuza -N)
 
 echo -e "active_mail_magazine_subscribers:${active_mail_magazine_subscribers}\nall_users:${rakuichi_rakuza_user_all}" |  mail -s "$subject" $mail_to
